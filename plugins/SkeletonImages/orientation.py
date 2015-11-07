06128665349ff63aeebd4e2e4e88a7b8bfa6d739
Christian Blum <christianb314@gmail.com>
2015-11-08 00:06:11 +0100

    added plugin used to rewrite the image classes according to the image format support

---
+++ plugins/SkeletonImages/orientation.py
@@ -0,0 +1,48 @@
+from pelican import signals
+from PIL import Image
+import logging
+
+logging.Logger(__name__)
+
+def isclose(a, b, rtol=1e-2):
+  db = abs(b)*float(rtol)
+  return a > b-db and a < b+db
+
+def rewrite_images(path, context):
+  with open(path, "r+") as infile:
+    base_path = path[:path.rfind("/")+1]
+    content = infile.readlines()
+    images = {}
+    for i, line in enumerate(content):
+      if "<img" in line:
+        img_start = line.find("<img")
+        if img_start >= 0:
+          source_start = line.find("src=", img_start)
+          source_start = line.find("\"", source_start) + 1
+          source_end = line.find("\"", source_start)
+          with Image.open(base_path+line[source_start:source_end]) as image:
+            images[i] = image.size
+    for i in images.keys():
+      line = content[i]
+      img_start = line.find("<img")
+      w = images[i][0]
+      h = images[i][1]
+      if w > h:
+        content[i] = line[:img_start+5] + "class=\"landscape\" " + line[img_start+5:]
+      elif isclose(w, h):
+        content[i] = line[:img_start+5] + "class=\"square\" " + line[img_start+5:]
+      else:
+        if isclose(h, 1.5*w):
+          content[i] = line[:img_start+5] + "class=\"portrait32\" " + line[img_start+5:]
+        elif isclose(h, 4.0/3.0*w):
+          content[i] = line[:img_start+5] + "class=\"portrait43\" " + line[img_start+5:]
+        elif isclose(h, 5.0/4.0*w):
+          content[i] = line[:img_start+5] + "class=\"portrait54\" " + line[img_start+5:]
+        else:
+          content[i] = line[:img_start+5] + "class=\"portrait\" " + line[img_start+5:]
+    infile.seek(0)
+    infile.truncate()
+    infile.writelines(content)
+
+def register():
+  signals.content_written.connect(rewrite_images)
